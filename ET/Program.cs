d8cd3d5f4a6a1b832e587e4646c6b898c939f654
PHAN DINH TUAN <pdtuan04@gmail.com>
2025-11-02 22:59:12 +0700

    fix_error

--- ET/Program.cs
+++ ET/Program.cs
@@ -107,18 +107,12 @@ builder.Services.AddAuthorization(options =>
 });
 var app = builder.Build();
 
-
+app.ApplyMigrations();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-else
-{
-    app.ApplyMigrations();
-    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 app.UseStaticFiles();
