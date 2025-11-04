1fd5cad52ebef3c61c53b8d9a51a340c24a7f5f7
PHAN DINH TUAN <pdtuan04@gmail.com>
2025-11-04 14:05:39 +0700

    add-ql-role

--- Libs/Service/AdminService.cs
+++ Libs/Service/AdminService.cs
@@ -1,23 +1,31 @@
-﻿using System;
+﻿using Libs.Data;
+using Libs.Entity;
+using Libs.Models;
+using Libs.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Libs.Data;
-using Libs.Entity;
-using Libs.Repositories;
 
 namespace Libs.Service
 {
     public class AdminService
     {
-        private ApplicationDbContext dbContext;
-        private IMoPhongRepository moPhongRepository;
-        private UserRepository userRepository;
-        public AdminService(ApplicationDbContext dbContext) {
+        private readonly ApplicationDbContext dbContext;
+        private readonly IMoPhongRepository moPhongRepository;
+        private readonly IUserRepository userRepository;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<User> userManager;
+        public AdminService(ApplicationDbContext dbContext, UserManager<User> userManager, RoleManager<IdentityRole> roleManager) {
             this.dbContext = dbContext;
             this.moPhongRepository = new MoPhongRepository(dbContext);
             this.userRepository = new UserRepository(dbContext);
+            this.userManager = userManager;
+            this.roleManager = roleManager;
         }
         public void Save()
         {
@@ -26,6 +34,10 @@ namespace Libs.Service
         public async Task<PageList<MoPhong>> GetPagedMoPhong(int pageNumber, int pageSize, string? search, string? sortCol, string sortDir)
         {
             return await moPhongRepository.GetPagedMoPhong(pageNumber, pageSize, search, sortCol, sortDir);
+        }
+        public async Task<PageList<User>> GetPagedUser(int pageNumber, int pageSize, string? search, string? sortCol, string sortDir)
+        {
+            return await userRepository.GetPagedUser(pageNumber, pageSize, search, sortCol, sortDir);
         }
         public async Task<int> GetTotalUserCountAsync()
         {
@@ -38,6 +50,56 @@ namespace Libs.Service
         public async Task<int> GetAllUsersCountAsync()
         {
             return await userRepository.GetAllUsersCountAsync();
+        }
+        public async Task SaveAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+        public async Task<int> SetRoleUser(Guid id, string Role)
+        {
+            var userFinder = await userManager.FindByIdAsync(id.ToString());
+            if (userFinder == null)
+            {
+                return 0;
+            }
+            if(!await roleManager.RoleExistsAsync(Role))
+            {
+                return 0;
+            }
+            var existingRoles = await userManager.GetRolesAsync(userFinder);
+
+            if (existingRoles.Any())
+            {
+                await userManager.RemoveFromRolesAsync(userFinder, existingRoles);
+            }
+            var result = await userManager.AddToRoleAsync(userFinder, Role);
+            return result.Succeeded ? 1 : 0;
+        }
+        public async Task<IEnumerable<IdentityRole>> GetRoleListAsync()
+        {
+            return await roleManager.Roles.ToListAsync();
+        }
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            return user;
+        }
+        public async Task<UserModel> GetUserByIdIncludeRolesAsync(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return null;
+            }
+            var roles = await userManager.GetRolesAsync(user);
+            return new UserModel
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                CreateAt = user.CreatedAt,
+                Role = roles
+            };
         }
     }
 }
